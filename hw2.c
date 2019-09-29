fe5e0a6a66a5020481f3d81d889dd3ca28ef9e4b
cinxdy <cinxdys2@gmail.com>
2019-09-30 00:27:06 +0900

    update hw2.c distinct ver

--- hw2.c
+++ hw2.c
@@ -3,53 +3,75 @@
 #include <stdio.h>
 #define N 5
 #define M 5
+#define K 5
 
 int main (){
     int x,y,k;
 
     // scan the input file
     int input[N][M];
-    char c;
+    char s;
 
     for (y = 0 ; y < N ; y++){
         for ( x = 0 ; x < M ; x++){
-            scanf("%c ",&c);
+            scanf("%c ",&s);
 
-            if(c=='?') input[y][x] = 0;
-            else input[y][x] = c - '0';
+            if(s=='?') input[y][x] = 0;
+            else input[y][x] = s - '0';
         }
     }
 
     // write formula
 	FILE * fp = fopen("formula", "w") ;
 
-	for (y = 1 ; y <= N ; y++)
-		for (x = 1 ; x <= M ; x++)
-			fprintf(fp, "(declare-const a%d%d Int)\n", y, x) ;
+    char c[5] = {'a','b','c','d','e'};
 
-    // 0<= a_yx <=1
-    for (y = 1 ; y <= N ; y++)
-		for (x = 1 ; x <= M ; x++)
-			fprintf(fp, "(assert (and (<= 0 a%d%d) (<= a%d%d 1))) \n", y, x, y, x) ;
+    for(int k = 0; k < K ; k++){
 
-    // Q0 : sum of its neighbors is itself (?)
-    for (y = 1 ; y <= N ; y++){
-        for (x = 1 ; x <= M ; x++){
-            if(input[y-1][x-1]>0){
-                fprintf(fp,"(assert (= (+ \n");
-                for(int m = -1 ; m <= 1 ; m++){
-                    for(int n = -1 ; n <= 1 ; n++){
-                        if(0<y+m && y+m<=N && 0<x+n && x+n<=M)
-                            fprintf(fp,"a%d%d ",y+m,x+n);
+        for (y = 1 ; y <= N ; y++)
+            for (x = 1 ; x <= M ; x++)
+                fprintf(fp, "(declare-const %c%d%d Int)\n", c[k],y, x) ;
+
+        // 0<= a_yx <=1
+        for (y = 1 ; y <= N ; y++)
+            for (x = 1 ; x <= M ; x++)
+                fprintf(fp, "(assert (and (<= 0 %c%d%d) (<= %c%d%d 1))) \n", c[k],y, x,c[k], y, x) ;
+
+        // Q0 : sum of its neighbors is itself (?)
+        for (y = 1 ; y <= N ; y++){
+            for (x = 1 ; x <= M ; x++){
+                if(input[y-1][x-1]>0){
+                    fprintf(fp,"(assert (= (+ \n");
+                    for(int m = -1 ; m <= 1 ; m++){
+                        for(int n = -1 ; n <= 1 ; n++){
+                            if(0<y+m && y+m<=N && 0<x+n && x+n<=M)
+                                fprintf(fp,"%c%d%d ",c[k],y+m,x+n);
+                        }
                     }
+                    fprintf(fp,") %d))\n",input[y-1][x-1]);
                 }
-                fprintf(fp,") %d))\n",input[y-1][x-1]);
             }
         }
     }
+    for(int kk = K ; kk > 0 ; kk--){
+        fprintf(fp,"(push)\n");
+        fprintf(fp,"(assert (or \n");
+        for (y = 1 ; y <= N ; y++){
+            for (x = 1 ; x <= M ; x++){
+                fprintf(fp,"(distinct ");
+                for (int k = 0 ; k < kk ; k++){
+                    fprintf(fp,"%c%d%d ",c[k],y,x);
+                }
+                fprintf(fp,")\n");
+            }
+        }
+        fprintf(fp,"))");
+    
 
-    // check Satisfiable and get model
-    fprintf(fp, "(check-sat)\n(get-model)\n") ;
+        // check Satisfiable and get model
+        fprintf(fp, "(check-sat)\n (get-model)\n");
+        fprintf(fp,"(pop)\n"); 
+    }
     
     fclose(fp);
 }
