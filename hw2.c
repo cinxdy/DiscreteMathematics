3abaa7a37db9a94c4ef43d65b2771b3c184ff67f
sungbin <21700613@handong.edu>
2019-10-02 05:17:32 +0900

    SB v3

--- hw2.c
+++ hw2.c
@@ -1,20 +1,27 @@
 // 21800409 Jiyoung Shin
+// 21700613 SB
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 // #define N 5
 // #define M 5
 #define T 5
+int board[T][1001][1001];
 
+void print_board(int tt);
+bool z3(int tt);
+int formula(int tt, int** input);
+int N, M;
 int main (){
     int x,y,k;
 
     // scan the input file
-    int N = 0, M = 0;
     int** input;
     int arr[4096];
     int arr_cnt = 0;
     char c;
-
+    N = 0; M = 0;
     while(!feof(stdin)) {
 	char ch;
 	scanf("%c",&ch);
@@ -35,27 +42,40 @@ int main (){
 	input[i] = (int*) malloc(sizeof(int)*M);
     }
 
-
     for(int i = 0; i < arr_cnt; i++) {
-
-
 	int quo, remain;
 	quo = i / M;
 	remain = i % M;
 	input[quo][remain] = arr[i];
     }
+    int tt;
+    for(tt = 0; tt < 5; tt ++) {
 
+	if(!formula(tt,input)) {
+		break;
+	} else {
+    		print_board(tt);
+	}
+    }
+    if(tt == 0) {
+	printf("No solution\n");
+    }
+}    
+
+
+    // check Satisfiable and get model
+int formula(int tt, int** input) {
+    int x,y,k;
     // write formula
 	FILE * fp = fopen("formula", "w") ;
-
 	for (y = 1 ; y <= N ; y++)
 		for (x = 1 ; x <= M ; x++)
-			fprintf(fp, "(declare-const a%d%d Int)\n", y, x) ;
+			fprintf(fp, "(declare-const a%d_%d Int)\n", y, x) ;
 
     // 0<= a_yx <=1
     for (y = 1 ; y <= N ; y++)
 		for (x = 1 ; x <= M ; x++)
-			fprintf(fp, "(assert (and (<= 0 a%d%d) (<= a%d%d 1))) \n", y, x, y, x) ;
+			fprintf(fp, "(assert (and (<= 0 a%d_%d) (<= a%d_%d 1))) \n", y, x, y, x) ;
 
     // Q0 : sum of its neighbors is itself (?)
     for (y = 1 ; y <= N ; y++){
@@ -65,16 +85,70 @@ int main (){
                 for(int m = -1 ; m <= 1 ; m++){
                     for(int n = -1 ; n <= 1 ; n++){
                         if(0<y+m && y+m<=N && 0<x+n && x+n<=M)
-                            fprintf(fp,"a%d%d ",y+m,x+n);
+                            fprintf(fp,"a%d_%d ",y+m,x+n);
                     }
                 }
                 fprintf(fp,") %d))\n",input[y-1][x-1]);
             }
         }
     }
+    // Q1 : never print same answer again
+    for(int t = 0; t < tt; t++) {
 
-    // check Satisfiable and get model
+	fprintf(fp, "(assert (not (and ");
+	for(y = 1 ; y <= N; y++)
+		for(x = 1 ; x <= M ; x++) {
+
+		int v = board[t][y-1][x-1];
+		fprintf(fp,"(= a%d_%d %d) ",y,x,v) ;
+	}
+	fprintf(fp,") )) \n") ;
+    }
+
     fprintf(fp, "(check-sat)\n(get-model)\n") ;
     
     fclose(fp);
+    return z3(tt);
+}
+bool z3(int tt) {
+    int i, j, k;
+    char satis[128];
+    char a[128] ;
+    char b[128] ;
+    char s[128] ;
+    char t[128] ;
+
+	FILE * fin = popen("z3 formula", "r");
+
+	while(!feof(fin)) {
+    		fscanf(fin,"%s",satis);
+		if(strcmp("unsat",satis) == 0) {
+			fscanf(fin,"%s",b); // error message absort
+			return false;
+		} else if(strcmp("sat",satis) == 0) {
+			fscanf(fin,"%s",b); // error message absort
+			while(1) {
+
+				int val, col, row;
+
+				fscanf(fin,"%s", a) ;
+				if(strcmp(")",a) == 0) break;
+				fscanf(fin," a%d_%d %s %s %s", &col,&row, b, b, t) ;
+
+				t[strlen(t)-1] = 0x0;
+				val = atoi(t);
+				board[tt][col-1][row-1] = val;		
+			}
+		}
+	}
+	return true;
+}
+void print_board(int tt) {
+	for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				printf("%d ",board[tt][i][j]);
+			}
+			printf("\n");
+		}
+	printf("\n");
 }
